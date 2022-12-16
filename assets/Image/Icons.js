f4d46da41969232e9d26127d73b7dad25e1cabd5
kyeltech <zikel1996@gmail.com>
2022-12-16 11:16:22 +0100

    first commit

---
+++ assets/Image/Icons.js
@@ -0,0 +1,11 @@
+import UserAvatar from './UserAvatar.svg';
+import FingerScan from './FingerScan.svg';
+import FingerPrint from './Fingerprint.svg';
+import Faceid from './Faceid.svg';
+
+export const Icons = {
+  UserAvatar,
+  FingerScan,
+  FingerPrint,
+  Faceid,
+};
